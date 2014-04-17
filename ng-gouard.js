3ba10f7b534ecfc69078fb6fd818d9fd5a76de1c
Paul-Emile MINY <paulemile.miny@gmail.com>
2014-04-17 10:20:04 +0200

    initial commit

---
+++ ng-gouard.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('ngOuardApp')
+  .directive('ngGouard', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('<img src="http://www.lanetscouade.com/sites/default/files/styles/profile_user_view/public/nicolas.png" alt="ng-gouard"/>');
+      }
+    };
+  });
